f81873dea9701472abd68ba149c3fcf4cdd4856f
Tanuffka <tanyaa.ischenko@gmail.com>
2023-04-28 00:23:33 +0300

    Add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const yearNow = new Date().getFullYear();
+
+export default function Footer() {
+    return (
+        <footer className='footer'>{yearNow} &copy; Tanuffka</footer>
+    );
+}
