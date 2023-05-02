945bc789706e07f0e46290b8fa39dc16e513ed18
Tanya <99911684+Tanuffka@users.noreply.github.com>
2023-05-02 23:31:53 +0300

    Change Header to AppBar and fix its styles (#5)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 import Footer from './components/Footer';
-import Header from './components/Header';
+import AppBar from './components/AppBar';
 import Content from './components/Content';
 
 export default function App() {
   return (
     <div className="app">
-      <Header />
+      <AppBar />
       <Content />
       <Footer />
     </div>
