bfa6580ec42eae7e180c70b7f8df9da6855c93e5
Tanuffka <tanyaa.ischenko@gmail.com>
2023-05-20 00:15:55 +0300

    Add multiple todo items

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,21 +1,20 @@
 import './styles.css';
+import { useState } from 'react';
 import Icon from '@mdi/react';
 import { mdiClose } from '@mdi/js';
 
-const todoItemText = 'Here is my 1st todo item Here is my 1st todo item Here is my 1st todo item Here is my 1st todo item';
-
-export default function TodoItem() {
+export default function TodoItem({description, completed}) {
     return (
         <div className="todoitem">
             <div className="checkbox">
-                <input type="checkbox" />
+                <input type="checkbox" checked={completed} readOnly/>
             </div>
             <div className="text">
-                <p title={todoItemText}>{todoItemText}</p>
+                <p title={description}>{description}</p>
             </div>
             <button title="Delete" className="button">
                 <Icon path={mdiClose} size={1} />
             </button>
         </div>
     );
-} 
+}
