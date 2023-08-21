4e81d2cce818faeb23541d24b07fb034e4696a65
Tanya <99911684+Tanuffka@users.noreply.github.com>
2023-08-21 01:11:51 -0600

    Move default todos to json file (#12)

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,33 +4,21 @@ import Card from '../Card';
 import PageTitle from '../PageTitle';
 import TodoCreator from '../TodoCreator';
 import TodoItem from '../TodoItem';
-import { useState } from 'react';
-
-const defaultTodos = [
-    {
-        id: 1,
-        description: 'Todo item #1, next will be #2',
-        completed: true,
-    },
-    {
-        id: 2,
-        description: 'Todo item #2, next will be #3',
-        completed: true,
-    },
-    {
-        id: 3,
-        description: 'Todo item #3, next will be #4',
-        completed: false,
-    },
-    {
-        id: 4,
-        description: 'Todo item #4, next will be #5',
-        completed: true,
-    },
-];
+import { useState, useEffect } from 'react';
 
 export default function Content() {
-    const [todos, setTodos] = useState(defaultTodos);
+    const [todos, setTodos] = useState([]);
+
+    useEffect (() => {
+        fetch('data/todos.json')
+            .then((data) => data.json())
+            .then((data) => {
+                setTodos(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
     return (
         <div className="content">
