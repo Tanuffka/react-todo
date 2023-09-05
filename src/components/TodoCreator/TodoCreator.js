c0e2aec7fd2e66f8c09623b77b513c85393ab70a
Tanya <99911684+Tanuffka@users.noreply.github.com>
2023-09-04 21:07:53 -0600

    Add possibility to create new todos (#13)

--- src/components/TodoCreator/TodoCreator.js
+++ src/components/TodoCreator/TodoCreator.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 import "./styles.css";
 
-export default function TodoCreator() {
-    return (
-        <div className="todocreator">
-            <input type="text" placeholder="Create your todo..."></input>
-            <button>Create</button>
-        </div>
-    );
-}
+export default function TodoCreator({ onCreateTodo }) {
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+
+  const handleCreate = () => {
+    if (text.trim() === "") return;
+
+    onCreateTodo(text);
+    setText("");
+  };
+
+  return (
+    <div className="todocreator">
+      <input 
+        type="text"
+        placeholder="Create your todo..."
+        value={text}
+        onChange={handleChange}
+      />
+      <button onClick={handleCreate}>Create</button>
+    </div>
+  );
+  
+  }
