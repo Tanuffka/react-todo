ec47730572038289f8c7d53f253c7a4cb4b14620
Tanya <99911684+Tanuffka@users.noreply.github.com>
2023-09-12 20:10:27 -0600

    Add possibility to edit todos description (#15)

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,18 +10,28 @@ export default function Content() {
   const [todos, setTodos] = useState([]);
 
   const handleCreateTodo = (description) => {
-    const newTask = {
+    const newTodo = {
       description,
       id: new Date().getTime(),
       completed: false,
     };
 
-    setTodos([...todos, newTask]);
+    setTodos([...todos, newTodo]);
   };
 
   const handleRemoveTodo = (id) => {
     const filteredTodos = todos.filter((todo) => todo.id !== id);
     setTodos(filteredTodos);
+  };
+
+  const handleEditTodo = (id, description) => {
+    const editedTodos = todos.map((todo) => {
+      if (id === todo.id) {
+        return { ...todo, description };
+      }
+      return todo;
+    });
+    setTodos(editedTodos);
   };
 
   useEffect(() => {
@@ -47,6 +57,7 @@ export default function Content() {
             description={todo.description}
             completed={todo.completed}
             onRemoveTodo={handleRemoveTodo}
+            onEditTodo={handleEditTodo}
           />
         ))}
       </Card>
