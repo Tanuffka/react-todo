f385d087bd1613b8ce1a08de7904184f31ab4c34
Tanuffka <tanyaa.ischenko@gmail.com>
2023-09-29 23:06:28 -0600

    Change todo creator input border width

---
+++ src/components/TodoCreator/styled.js
@@ -0,0 +1,8 @@
+import { styled } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+export const StyledOutlinedInput = styled(OutlinedInput)`
+  & .MuiOutlinedInput-notchedOutline {
+    border-width: 2px;
+  }
+`;
