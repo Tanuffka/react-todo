33465e2f50271197aaa46bbcde8110173a5ded44
Tanya <99911684+Tanuffka@users.noreply.github.com>
2023-12-16 22:04:22 -0700

    Add loader indicator for todo item features (#33)

--- src/pages/Home/components/TodoItem/TodoItem.tsx
+++ src/pages/Home/components/TodoItem/TodoItem.tsx
@@ -2,19 +2,22 @@ import { ChangeEvent, useState } from 'react';
 
 import Icon from '@mdi/react';
 import { mdiDelete, mdiPencil } from '@mdi/js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import Dialog from '@mui/material/Dialog';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-import { AppDispatch } from 'src/redux/store';
+import { AppDispatch, RootState } from 'src/redux/store';
 import { editTask, removeTask } from 'src/redux/tasks';
 
 // import { useTodoContext } from '../../context/Todo';
@@ -30,6 +33,9 @@ interface Props {
 export default function TodoItem({ id, description, completed }: Props) {
   // const { editTodo, changeTodoStatus } = useTodoContext();
   const dispatch = useDispatch<AppDispatch>();
+  const { editingTaskId, removingTaskId } = useSelector(
+    (state: RootState) => state.tasks
+  );
 
   const [isHovered, setIsHovered] = useState(false);
   const [open, setOpen] = useState(false);
@@ -46,8 +52,11 @@ export default function TodoItem({ id, description, completed }: Props) {
     }
 
     // editTodo(id, newDescription);
-    dispatch(editTask({ _id: id, description: newDescription, completed }));
-    setOpen(false);
+    dispatch(
+      editTask({ _id: id, description: newDescription, completed })
+    ).then(() => {
+      setOpen(false);
+    });
   };
 
   const handleStatusChange = () => {
@@ -73,9 +82,17 @@ export default function TodoItem({ id, description, completed }: Props) {
     setOpen(true);
   };
 
-  const handleEditClose = () => {
+  const handleEditCancel = () => {
     setNewDescription(description);
     setOpen(false);
+  };
+
+  const handleEditClose: DialogProps['onClose'] = (_event, reason) => {
+    if (reason === 'backdropClick' || reason === 'escapeKeyDown') {
+      return;
+    }
+
+    handleEditCancel();
   };
 
   const descriptionTextDecoration = completed ? 'line-through' : 'none';
@@ -84,11 +101,31 @@ export default function TodoItem({ id, description, completed }: Props) {
     ? 'Mark as uncompleted'
     : 'Mark as completed';
 
+  const isEditing = id === editingTaskId;
+  const isDeleting = id === removingTaskId;
+
   return (
     <StyledBox onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <Tooltip title={completedTextHelper} placement="left">
-        <Checkbox checked={completed} onChange={handleStatusChange} />
-      </Tooltip>
+      <Box
+        sx={{
+          height: 42,
+          width: 42,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {isEditing && <CircularProgress size={20} thickness={5} />}
+        {!isEditing && (
+          <Tooltip title={completedTextHelper} placement="left">
+            <Checkbox
+              checked={completed}
+              disabled={isEditing || isDeleting}
+              onClick={handleStatusChange}
+            />
+          </Tooltip>
+        )}
+      </Box>
       <StyledDescription sx={{ textDecoration: descriptionTextDecoration }}>
         {description}
       </StyledDescription>
@@ -103,26 +140,65 @@ export default function TodoItem({ id, description, completed }: Props) {
           />
         </DialogContent>
         <DialogActions>
-          <Button variant="outlined" onClick={handleEditClose}>
+          <Button
+            variant="outlined"
+            disabled={isEditing}
+            onClick={handleEditCancel}
+          >
             Cancel
           </Button>
-          <Button variant="contained" onClick={handleSave}>
-            Save
+          <Button
+            variant="contained"
+            disabled={isEditing}
+            sx={{
+              minWidth: 80,
+            }}
+            onClick={handleSave}
+          >
+            {isEditing && (
+              <CircularProgress
+                size={25}
+                sx={{
+                  color: 'white',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                }}
+              />
+            )}
+            {!isEditing && <Typography>Save</Typography>}
           </Button>
         </DialogActions>
       </Dialog>
       {isHovered && (
         <>
           <Tooltip title="Edit" placement="bottom">
-            <IconButton onClick={handleEditOpen}>
+            <IconButton disabled={isDeleting} onClick={handleEditOpen}>
               <Icon path={mdiPencil} size={1} />
             </IconButton>
           </Tooltip>
-          <Tooltip title="Delete" placement="bottom">
-            <IconButton color="primary" onClick={handleRemove}>
-              <Icon path={mdiDelete} size={1} color="grey" />
-            </IconButton>
-          </Tooltip>
+          <Box
+            sx={{
+              height: 42,
+              width: 42,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {isDeleting && <CircularProgress size={20} thickness={5} />}
+            {!isDeleting && (
+              <Tooltip title="Delete" placement="bottom">
+                <IconButton
+                  color="primary"
+                  disabled={isDeleting}
+                  onClick={handleRemove}
+                >
+                  <Icon path={mdiDelete} size={1} color="grey" />
+                </IconButton>
+              </Tooltip>
+            )}
+          </Box>
         </>
       )}
     </StyledBox>
